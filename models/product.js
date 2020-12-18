efa5cb55e9bd9db65448f8e73098bdfe6aa3a47b
jhonsnow456 <thakuraman22july@gmail.com>
2020-12-18 10:36:13 +0530

    connected models with controllers

--- models/product.js
+++ models/product.js
@@ -1,16 +1,40 @@
-const products =[]
+const fs = require('fs')
+const path = require('path')
 
 
+// set a global helper function 
+const p = path.join(
+    path.dirname(require.main.filename), 
+    'data', 
+    'products.json'
+);
+
+const getProductsFromFile = call_back =>{
+    fs.readFile(p, (err, fileContent)=>{
+        if (err){
+            return call_back([]);
+        }
+        return call_back(JSON.parse(fileContent));
+    });
+}
+
+// created a class named Product and defined it's properties
 module.exports = class Product{
-    Product(title){
+    constructor(title){
         this.title = title;
     }
 
-    save(){
-        products.push(this);
+    save(){  
+        getProductsFromFile(products =>{
+            products.push(this);
+            
+            fs.writeFile(p, JSON.stringify(products), (err)=>{
+                console.log(err);
+            });
+        });
     }
 
-    static fetchAll(){
-        return products;
+    static fetchAll(call_back){
+        getProductsFromFile(call_back);
     }
 }
