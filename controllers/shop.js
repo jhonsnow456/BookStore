b7ff598e93527f7d545208e5bdd56ce3bf28ee03
jhonsnow456 <thakuraman22july@gmail.com>
2020-12-21 10:37:44 +0530

    created add to product post request for id

--- controllers/shop.js
+++ controllers/shop.js
@@ -27,6 +27,12 @@ exports.getCart = (req, res, next) => {
     });
 }
 
+exports.postCart = (req, res, next) => {
+    const prodId = req.body.productId;
+    console.log(prodId);
+    res.redirect('/cart');
+}
+
 exports.getOrders = (req, res, next) => {
     Product.fetchAll(products => {
         res.render('shop/orders', {prods: products, pageTitle: 'Your Cart', path: '/orders' });
