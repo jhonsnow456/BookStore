a3e94c15a60b555b4798ca0178c9ea0766bcb740
jhonsnow456 <thakuraman22july@gmail.com>
2020-12-22 13:57:12 +0530

    added getEditProduct and postEdiProduct

--- controllers/admin.js
+++ controllers/admin.js
@@ -3,8 +3,8 @@ const Product = require('../models/product');
 
 exports.getAddProduct = (req, res, next)=>{
     // add the form over here 
-    res.render('admin/add-product', { pageTitle: 'admin add-product', path: '/admin/add-product' });
-}
+    res.render('admin/edit-product', { pageTitle: 'admin add-product', path: '/admin/add-product', editing: false });
+};
 
 exports.postAddProduct = (req, res, next) => {
     const title = req.body.title;
@@ -12,11 +12,40 @@ exports.postAddProduct = (req, res, next) => {
     const price = req.body.price;
     const description = req.body.description;
 
-    const product = new Product(title, imageUrl, price, description);
+    const product = new Product(null, title, imageUrl, price, description);
     
     product.save();
     res.redirect('/'); // to redirect to the root
 }
+
+exports.getEditProduct = (req, res, next) => {
+    const editMode = req.query.edit;
+    
+    if (!editMode) {
+      return res.redirect('/');
+    }
+    
+    const prodId = req.params.productId;
+    
+    Product.findById(prodId, product => {
+        if (!product) {
+            return res.redirect('/');
+        }
+        res.render('admin/edit-product', {pageTitle: 'Edit Product', path: '/admin/edit-product', editing: editMode, product: product });
+    });
+};
+  
+exports.postEditProduct = (req, res, next) => {
+    const prodId = req.body.productId;
+    const updatedTitle = req.body.title;
+    const updatedImageUrl = req.body.imageUrl;
+    const updatedPrice = req.body.price;
+    const updatedDesc = req.body.description;
+    
+    const updatedProduct = new Product(prodId, updatedTitle, updatedImageUrl, updatedDesc, updatedPrice);
+    updatedProduct.save();
+    res.redirect('/admin/products');
+};
 
 exports.getProducts = (req, res, next) => {
     Product.fetchAll(products => {
