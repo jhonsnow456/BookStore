ca607474371e384bc89ba97205b325224fb46072
unknown <thakuraman22july@gmail.com>
2020-12-24 14:13:18 +0530

    major change connection sucessful to mysql db

--- models/product.js
+++ models/product.js
@@ -1,74 +1,29 @@
-const fs = require('fs')
-const path = require('path')
+const db = require('../util/database');
 
-const Cart = require('./cart')
+const Cart = require('./cart');
 
-// set a global helper function 
-const p = path.join(path.dirname(require.main.filename), 'data', 'products.json');
+module.exports = class Product {
+  constructor(id, title, imageUrl, description, price) {
+    this.id = id;
+    this.title = title;
+    this.imageUrl = imageUrl;
+    this.description = description;
+    this.price = price;
+  }
 
-const getProductsFromFile = call_back =>{
-    fs.readFile(p, (err, fileContent)=>{
-        if (err){
-            return call_back([]);
-        }
-        return call_back(JSON.parse(fileContent));
-    });
-}
+  save() {
+   
+  }
 
-// created a class named Product and defined it's properties
-module.exports = class Product{
-    constructor(id, title, imageUrl, description, price) {
-        this.id = id;
-        this.title = title;
-        this.imageUrl = imageUrl;
-        this.price = price;
-        this.description = description;
-    }
+  static deleteById(id) {
+    
+  }
 
-    save(){  
-        getProductsFromFile(products =>{
-            if (this.id){
-                const existingProductIndex = products.findIndex(prod => prod.id === this.id);
-                const updatedProducts = [...products];
-                updatedProducts[existingProductIndex] = this;
+  static fetchAll() {
+    return db.execute('SELECT * FROM products');
+  }
 
-                fs.writeFile(p, JSON.stringify(updatedProducts), (err)=>{
-                    console.log(err);
-                });
-            }
-            else{
-                this.id = Math.random().toString();
-                products.push(this);
-            
-                fs.writeFile(p, JSON.stringify(products), (err)=>{
-                    console.log(err);
-                });
-            }
-        });
-    }
-
-    /** delete product by id */
-    static deleteById(id) {
-        getProductsFromFile(products => {
-            const product = products.find(prod => prod.id === id);
-            const updatedProducts = products.filter(prod => prod.id !== id);
-          
-            fs.writeFile(p, JSON.stringify(updatedProducts), err => {
-                if (!err) {
-                    Cart.deleteProduct(id, product.price);
-                }
-            });
-        });
-    }
-
-    static fetchAll(call_back){
-        getProductsFromFile(call_back);
-    }
-
-    static findById(id, call_back){
-        getProductsFromFile(products => {
-           const product = products.find(p => p.id === id);
-           call_back(product); 
-        });
-    }
-}
+  static findById(id) {
+    
+  }
+};
--- routes/admin.js
+++ routes/admin.js
@@ -1,29 +1,24 @@
-/** imported our express library + core packages*/ 
+const path = require('path');
+
 const express = require('express');
 
-
-/** Add controller function */
 const adminController = require('../controllers/admin');
-
 
 const router = express.Router();
 
-
-/** Adding the product */
-// /admin/add-product => GET request
+// /admin/add-product => GET
 router.get('/add-product', adminController.getAddProduct);
-// /admin/products => GET request
+
+// /admin/products => GET
 router.get('/products', adminController.getProducts);
 
-/** Display the product entry */
-// /admin/add-product => POST request
+// /admin/add-product => POST
 router.post('/add-product', adminController.postAddProduct);
 
-/** Added edit product route by id */
 router.get('/edit-product/:productId', adminController.getEditProduct);
+
 router.post('/edit-product', adminController.postEditProduct);
 
 router.post('/delete-product', adminController.postDeleteProduct);
 
-// export the packages 
-module.exports = router
+module.exports = router;
