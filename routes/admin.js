ebe3d9aa7ca084fa5d29b2c390ddbbfb2424af1b
jhonsnow456 <thakuraman22july@gmail.com>
2022-11-22 21:56:30 +0530

    removed unnessary commentsd

--- routes/admin.js
+++ routes/admin.js
@@ -1,29 +1,17 @@
-/** imported our express library + core packages*/ 
-const express = require('express');
-
+/** imported our express library + core packages*/
+const express = require("express");
 
 /** Add controller function */
-const adminController = require('../controllers/admin');
-
+const adminController = require("../controllers/admin");
 
 const router = express.Router();
 
+router.get("/add-product", adminController.getAddProduct);
+router.get("/products", adminController.getProducts);
+router.post("/add-product", adminController.postAddProduct);
+router.get("/edit-product/:productId", adminController.getEditProduct);
+router.post("/edit-product", adminController.postEditProduct);
+router.post("/delete-product", adminController.postDeleteProduct);
 
-/** Adding the product */
-// /admin/add-product => GET request
-router.get('/add-product', adminController.getAddProduct);
-// /admin/products => GET request
-router.get('/products', adminController.getProducts);
-
-/** Display the product entry */
-// /admin/add-product => POST request
-router.post('/add-product', adminController.postAddProduct);
-
-/** Added edit product route by id */
-router.get('/edit-product/:productId', adminController.getEditProduct);
-router.post('/edit-product', adminController.postEditProduct);
-
-router.post('/delete-product', adminController.postDeleteProduct);
-
-// export the packages 
-module.exports = router
+// export the packages
+module.exports = router;
--- routes/shop.js
+++ routes/shop.js
@@ -1,23 +1,20 @@
-/** imported our express library + core packages*/ 
-const express = require('express');
-
+/** imported our express library + core packages*/
+const express = require("express");
 
 /** Add controller function */
-const shopController = require('../controllers/shop');
-
+const shopController = require("../controllers/shop");
 
 const router = express.Router();
 
-
 /** Our home page */
-router.get('/', shopController.getIndex);
-router.get('/products', shopController.getProducts);
-router.get('/products/:productId', shopController.getProduct);
-router.get('/cart', shopController.getCart);
-router.post('/cart', shopController.postCart);
-router.post('/cart-delete-item', shopController.postCartDeleteProduct);
-router.get('/orders', shopController.getOrders);
-router.get('/checkout', shopController.getCheckout);
+router.get("/", shopController.getIndex);
+router.get("/products", shopController.getProducts);
+router.get("/products/:productId", shopController.getProduct);
+router.get("/cart", shopController.getCart);
+router.post("/cart", shopController.postCart);
+router.post("/cart-delete-item", shopController.postCartDeleteProduct);
+router.get("/orders", shopController.getOrders);
+router.get("/checkout", shopController.getCheckout);
 
 // export the package
 module.exports = router;
