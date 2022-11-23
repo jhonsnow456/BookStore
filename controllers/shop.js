4af0ecad75bccf09f30c6944a6cac9ae0ba044c7
jhonsnow456 <thakuraman22july@gmail.com>
2022-11-23 15:12:28 +0530

    created query to get product details

--- controllers/shop.js
+++ controllers/shop.js
@@ -2,7 +2,6 @@ const Product = require("../models/product");
 const Cart = require("../models/cart");
 
 exports.getProducts = (req, res, next) => {
-  // console.log('shop.js', adminData.products);
   Product.fetchAll()
     .then(([rows, fieldData]) => {
       res.render("shop/product-list", {
@@ -16,13 +15,15 @@ exports.getProducts = (req, res, next) => {
 
 exports.getProduct = (req, res, next) => {
   const prodId = req.params.productId;
-  Product.findById(prodId, (product) => {
-    res.render("shop/product-detail", {
-      product: product,
-      pageTitle: product.title,
-      path: "/products",
-    });
-  });
+  Product.findById(prodId)
+    .then(([product]) => {
+      res.render("shop/product-detail", {
+        product: product[0],
+        pageTitle: product[0].title,
+        path: "/products",
+      });
+    })
+    .catch((err) => err);
 };
 
 exports.getIndex = (req, res, next) => {
