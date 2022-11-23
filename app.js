ec1eedcb4cc382d2abb22792cae79be72056a820
jhonsnow456 <thakuraman22july@gmail.com>
2022-11-23 10:11:04 +0530

    created product controller to get product and to post by adding product on admin route

--- app.js
+++ app.js
@@ -1,40 +1,41 @@
-/** imported our express library + core packages*/ 
-const express = require('express');
-const bodyParser = require('body-parser');
-const path = require('path');
+/** imported our express library + core packages*/
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
 
 /** Add controller function */
-const errorController = require('./controllers/error');
-
+const errorController = require("./controllers/error");
 
 /** import defined files by me */
-const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
-
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
 
 /** create our express app */
 const app = express();
 
 /** set templating engine */
-app.set('view engine', 'pug');
-app.set('views', './views');
+app.set("view engine", "pug");
+app.set("views", "./views");
 
-
-/** Parsing the code we require a parser to check whether what kind of data is given 
- * to be delt with 
+/** Parsing the code we require a parser to check whether what kind of data is given
+ * to be delt with
  */
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 /** Serving static pages */
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-/** defining the routes */ 
-app.use('/admin', adminRoutes); // adding admin filter
+/** defining the routes */
+app.use("/admin", adminRoutes); // adding admin filter
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-// create a server 
-app.listen(process.env.PORT || 3000, function(){
-	console.log("Express server on port %d in %s mode", this.address().port, app.settings.env);
+// create a server
+app.listen(process.env.PORT || 3000, function () {
+  console.log(
+    "Express server on port %d in %s mode",
+    this.address().port,
+    app.settings.env
+  );
 });
