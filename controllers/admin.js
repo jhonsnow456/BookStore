9fd6706de63a0c4f05001c7d0ad92b770db87d55
jhonsnow456 <thakuraman22july@gmail.com>
2022-11-24 10:41:46 +0530

    user product association completed

--- controllers/admin.js
+++ controllers/admin.js
@@ -14,6 +14,9 @@ exports.postAddProduct = (req, res, next) => {
   const price = req.body.price;
   const description = req.body.description;
 
+  // `createProduct()` method is created by sequalize,
+  // since there was an association between user and product created in app.js
+  // that the product belong to user
   req.user
     .createProduct({
       title: title,
@@ -34,8 +37,12 @@ exports.getEditProduct = (req, res, next) => {
     return res.redirect("/");
   }
   const prodId = req.params.productId;
-  Product.findByPk(prodId)
-    .then((product) => {
+
+  req.user
+    .getProducts({ where: { id: prodId } })
+    // Product.findByPk(prodId)
+    .then((products) => {
+      const product = products[0];
       if (!product) {
         return res.redirect("/");
       }
@@ -73,7 +80,9 @@ exports.postEditProduct = (req, res, next) => {
 };
 
 exports.getProducts = (req, res, next) => {
-  Product.findAll()
+  req.user
+    .getProducts()
+    // Product.findAll()
     .then((products) => {
       res.render("admin/products", {
         prods: products,
