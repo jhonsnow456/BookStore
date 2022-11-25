74004c996cf9524d7b34621c2fdca2180e578618
jhonsnow456 <thakuraman22july@gmail.com>
2022-11-25 21:18:04 +0530

    sequelize project completed

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const path = require("path");
+const dotenv = require("dotenv").config();
 
 /** Add controller function */
 const errorController = require("./controllers/error");
@@ -12,6 +13,8 @@ const Product = require("./models/product");
 const User = require("./models/user");
 const Cart = require("./models/cart");
 const CartItem = require("./models/cart-item");
+const Order = require("./models/order");
+const OrderItem = require("./models/oder-item");
 
 /** import defined files by me */
 const adminRoutes = require("./routes/admin");
@@ -56,6 +59,10 @@ Cart.belongsTo(User);
 
 Cart.belongsToMany(Product, { through: CartItem });
 Product.belongsToMany(Cart, { through: CartItem });
+
+Order.belongsTo(User);
+User.hasMany(Order);
+Order.belongsToMany(Product, { through: OrderItem });
 
 sequelize
   // .sync({ force: true })
